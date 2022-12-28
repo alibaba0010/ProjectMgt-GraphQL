8c34fef516acbcd664b0c483bbaee0595668ced4
alibaba0010 <zakariyahali100@gmail.com>
2022-12-28 02:43:54 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+cd server/
+
+npm install
+
+npm i express express-graqhql graqhql mongoose cors
+npm i -D nodemon
---
+++ server/src/server.js
@@ -0,0 +1,21 @@
+console.log("In server");
+
+// import { createServer } from "http";
+// import dotenv from "dotenv";
+// import connectDB from "./db.js";
+// import app from "./app.js";
+
+// dotenv.config();
+// const PORT = process.env.PORT || 2000;
+
+// const uri = process.env.MONGO_URL;
+
+// const server = createServer(app);
+
+// async function startServer() {
+//   await connectDB(uri);
+//   server.listen(PORT, () => console.log(`Listening to port ${PORT}`));
+// }
+
+// startServer();
+  
