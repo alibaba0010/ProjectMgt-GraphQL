75c2d9aa7f57e9eb23898cbf06e7a91bae09a18a
alibaba0010 <zakariyahali100@gmail.com>
2023-01-04 09:45:19 +0100

    Final commit

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import { graphqlHTTP } from "express-graphql";
 import { schema } from "./schema/schema.js";
@@ -10,10 +11,10 @@ const app = express();
 
 app
   // .use(json())
+  .use(cors())
   .use(
     "/graphql",
     graphqlHTTP({ schema, graphiql: process.env.NODE_ENV === "development" })
   );
 
 export default app;
-
